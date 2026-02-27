0619f179fbb1c8ec1d92e275c686d50b59547244
ZdravoHvalaKesaNeTreba <m.mironov.ads@gmail.com>
2026-02-27 19:47:50 +0300

    Добавлен год 2026 в футер

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center pt-12 pb-8 border-t border-white/10 mt-12 gap-6">
       <div>
         <span className="text-[#9CA3AF] text-base">
-          Приносите замечания и идеи по улучшению -{' '}
+          © 2026 Приносите замечания и идеи по улучшению -{' '}
         </span>
         <a 
           href="https://t.me/yndx_mironovmax" 
